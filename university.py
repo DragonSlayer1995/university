d72f3118c55f7839134d683821c2a1ae23220df5
Pavel Strakhov <p.strakhov4work@gmail.com>
2021-08-28 13:53:13 +0300

    Create file writer for specific department

--- university.py
+++ university.py
@@ -22,9 +22,9 @@ for _ in range(3):
         sorted_applicant = sorted(sorted_applicant,
                                   key=lambda x: (-((float(x[2]) + float(x[3])) / 2) if faculty == 'Biotech'
                                                  else -((float(x[4]) + float(x[5])) / 2) if faculty == 'Engineering'
-                                                 else -((float(x[2]) + float(x[4])) / 2) if faculty == 'Physics'
-                                                 else -float(x[3]) if faculty == 'Chemistry'
-                                                 else -float(x[4]),
+                                  else -((float(x[2]) + float(x[4])) / 2) if faculty == 'Physics'
+                                  else -float(x[3]) if faculty == 'Chemistry'
+                                  else -float(x[4]),
                                                  x[0] + ' ' + x[1]))
         faculty_list = [x for x in sorted_applicant if x[i] == faculty]
         maximum_students = maximum_students
@@ -36,17 +36,24 @@ for _ in range(3):
         sorted_applicant = [x for x in sorted_applicant if x not in faculty_list and x not in faculty_list]
     i += 1
 
+for key, value in result.items():
+    for student in value:
+        if key == 'Biotech':
+            student[2] = (float(student[2]) + float(student[3])) / 2
+        if key == 'Engineering':
+            student[2] = (float(student[4]) + float(student[5])) / 2
+        if key == 'Physics':
+            student[2] = (float(student[2]) + float(student[4])) / 2
+        if key == 'Chemistry':
+            student[2] = float(student[3])
+        if key == 'Mathematics':
+            student[2] = float(student[4])
+
 for key in result.keys():
-    new_list = sorted(result[key], key=lambda x: (-float(x[science_indexes[key]]), x[0] + ' ' + x[1]))
+    new_list = sorted(result[key], key=lambda x: (-float(x[2]), x[0] + ' ' + x[1]))
     result[key] = new_list
 
 for key, value in result.items():
-    print(key)
-    for student in value:
-        print(student[0], student[1], (float(student[2]) + float(student[3])) / 2 if key == 'Biotech'
-              else ((float(student[4]) + float(student[5])) / 2) if key == 'Engineering'
-              else ((float(student[2]) + float(student[4])) / 2) if key == 'Physics'
-              else float(student[3]) if key == 'Chemistry'
-              else float(student[4])
-              )
-    print()
+    with open(key.lower() + '.txt', 'w', encoding='utf-8') as out_file:
+        for student in value:
+            out_file.write(student[0] + ' ' + student[1] + ' ' + str(student[2]) + '\n')
